8f2a996e806208a46aed317668186508409fb78a
wamuni <boshi.pan@outlook.com>
2024-01-22 23:10:05 +0000

    type function global variable and local variable how to pring multiple line case-sensitive

--- main.lua
+++ main.lua
@@ -16,3 +16,41 @@ print(y + 0.1)
 
 _G.GlobalVar = 3 -- can be declared without _G. global variable, which can be accessed from other file
 print(GlobalVar)
+
+--[[
+    nil.. means nothing, can be treated as false
+    number.. 1, 1.2
+    string.. 'hello', "hello world"
+    boolean.. true or false
+    table.. very special data structure in lua
+]]
+
+local name = "Eddie"
+print("My name is"..name.." this is a the lua learn project and I "..name.." will done it really well")
+
+
+local suername = "Pan"
+print(name.." "..suername)
+name = nil -- use for optimizing the program, make it faster?
+
+print([[
+     coke
+    cola
+    eddie
+]])
+
+-- normally, the global variable starts with uppercase letter
+-- which is not nesscary, however, this is rule unwritten
+C = 20
+local c = 11
+print(C, c) -- lua is case-sensitive
+_G.Hello = "Hello" -- another way to declare a global variable in Lua
+
+local what_type = 1
+print(type(what_type)) -- number
+what_type = "string" 
+print(type(what_type)) -- string
+what_type = false
+print(type(what_type)) -- boolean
+what_type = nil
+print(type(what_type)) -- nil
